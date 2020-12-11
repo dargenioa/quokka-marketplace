f50e6871bfc5904a73d07613822dbda02e947d65
Amanda D'Argenio <dargenioa14@gmail.com>
2020-12-11 17:41:12 -0500

    init models and config folders and files for sequelize

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-// const db = require('./models');
+const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
