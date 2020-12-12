6d01a7312bbae0bdde38c94f3939c091b9e3de53
Christopher-Lamb <chris.lamb13@hotmail.com>
2020-12-12 08:00:57 -0800

    Small changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,30 +2,32 @@ const db = require("../models");
 const passport = require("../config/passport");
 const router = require("express").Router();
 
-router.get("/api/user", function(req, res) {
-    if (!req.user) {
-        res.json({});
-    } else {
-        res.json({
-            email: req.user.email,
-            id: req.user.id
-        });
-    }
+router.get("/api/user", function (req, res) {
+  if (!req.user) {
+    res.json({});
+  } else {
+    res.json({
+      email: req.user.email,
+      id: req.user.id,
+    });
+  }
 });
 
-router.post("/api/signup", function(req, res) {
-    db.User.create({
-        email: req.body.email,
-        password: req.body.password
-    }).then(function(){
-        res.redirect(307, "api/login");
-    }).catch(function(err) {
-        res.status(401).json(err);
+router.post("/api/signup", function (req, res) {
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then(function () {
+      res.redirect(307, "/api/login");
+    })
+    .catch(function (err) {
+      res.status(401).json(err);
     });
 });
 
-router.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
+router.post("/api/login", passport.authenticate("local"), function (req, res) {
+  res.json(req.user);
 });
 
 module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,29 +2,27 @@ const path = require("path");
 const router = require("express").Router();
 const authenticated = require("../config/middleware/authenticated");
 
-
 router.get("/", function (req, res) {
-    if (req.user) {
-        res.redirect("/members")
-    } else {
-        res.sendFile(path.join(__dirname, "../public/signup.html"));
-    }
+  if (req.user) {
+    res.redirect("/members");
+  } else {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  }
 });
 router.get("/login", function (req, res) {
-    if (req.user) {
-        res.redirect("/members")
-    } else {
-        res.sendFile(path.join(__dirname, "../public/login.html"));
-    }
+  if (req.user) {
+    res.redirect("/members");
+  } else {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  }
 });
 
 router.get("/members", authenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  res.sendFile(path.join(__dirname, "../public/members.html"));
 });
 
 router.get("*", authenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  res.sendFile(path.join(__dirname, "../public/signup.html"));
 });
-
 
 module.exports = router;
