abc698e5008adc424790ad29f6cd1e592bd95e97
Amanda D'Argenio <dargenioa14@gmail.com>
2020-12-11 19:18:32 -0500

    added authenticated middleware and redirect to html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,21 +1,25 @@
 const path = require("path");
 const router = require("express").Router();
+const authenticated = require("../config/middleware/authenticated");
 
 
+router.get("/", function (req, res) {
+    if (req.user) {
+        res.redirect("/members")
+    } else {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    }
+});
 router.get("/login", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    if (req.user) {
+        res.redirect("/members")
+    } else {
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    }
 });
 
-router.get("/signup", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-});
-
-router.get("/member", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/member.html"));
-});
-
-router.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+router.get("/members", authenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
 });
 
 module.exports = router;
