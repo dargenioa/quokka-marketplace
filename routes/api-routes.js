439f13c176fbf528fed357c013fb4790e7d58ecf
Christopher-Lamb <chris.lamb13@hotmail.com>
2020-12-13 17:38:38 -0800

    Formatting and signup.html route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,62 +1,58 @@
 const db = require("../models");
 const passport = require("../config/passport");
 const router = require("express").Router();
-require('dotenv').config();
 
+require("dotenv").config();
+
+//API Key not is use yet possibly for Walmart API
 const apiKey = process.env.API_KEY;
 console.log(apiKey);
 
-<<<<<<< HEAD
-router.get("/api/user", function (req, res) {
+//GET user information to be used
+router.get("/api/user_data", function (req, res) {
+ //If user not logged in return empty string
   if (!req.user) {
     res.json({});
   } else {
+   //Else send their information
     res.json({
       email: req.user.email,
       id: req.user.id,
     });
   }
-=======
-router.get("/api/user_data", function (req, res) {
-    if (!req.user) {
-        res.json({});
-    } else {
-        res.json({
-            email: req.user.email,
-            id: req.user.id
-        });
-    }
->>>>>>> 7eb43a973c07202f2361394769a5f0d535d4e286
 });
 
-router.get("/api/listing", function (req, res) {
-    db.Listing.findAll({}).then(function (listing) {
-        res.json(listing);
-    });
+//Returns Listings table information
+router.get("/api/listings", function (req, res) {
+  db.Listing.findAll({}).then(function (listing) {
+    res.json(listing);
+  });
 });
 
-router.post("/api/listing", function (req, res) {
-    db.Listing.create(req.body).then(function (listing) {
-        res.json(listing);
-    });
+//Post a listing to Listing table in db
+router.post("/api/listings", function (req, res) {
+  db.Listing.create(req.body).then(function (listing) {
+    res.json(listing);
+  });
 });
+
+//Posts User information through Sign Up form
 router.post("/api/signup", function (req, res) {
-    db.User.create({
-        email: req.body.email,
-        password: req.body.password
-    }).then(function () {
-        res.redirect(307, "/api/login");
-    }).catch(function (err) {
-        res.status(401).json(err);
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then(function () {
+      res.redirect(307, "/api/login");
+    })
+    .catch(function (err) {
+      res.status(401).json(err);
     });
 });
 
+//Checks if login information is valid returns user information
 router.post("/api/login", passport.authenticate("local"), function (req, res) {
-<<<<<<< HEAD
   res.json(req.user);
-=======
-    res.json(req.user);
->>>>>>> 7eb43a973c07202f2361394769a5f0d535d4e286
 });
 
 module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,25 +2,33 @@ const path = require("path");
 const router = require("express").Router();
 const authenticated = require("../config/middleware/authenticated");
 
+//Upon Entry to site
 router.get("/", function (req, res) {
   if (req.user) {
-    res.redirect("/members");
+    res.redirect("/home");
   }
   res.sendFile(path.join(__dirname, "../public/signup.html"));
 });
+
+//Upon login
 router.get("/login", function (req, res) {
   if (req.user) {
-    res.redirect("/members");
+    res.redirect("/home");
   }
   res.sendFile(path.join(__dirname, "../public/login.html"));
 });
-
-router.get("/members", authenticated, function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/members.html"));
+//Send User to signup.html
+router.get("/signup", function (req, res) {
+  if (req.user) {
+    res.redirect("/home");
+  }
+  res.sendFile(path.join(__dirname, "../public/signup.html"));
 });
 
-// router.get("*", authenticated, function (req, res) {
-//   res.sendFile(path.join(__dirname, "../public/signup.html"));
-// });
+//GET to home checks to see if logged in send to home if no errors
+router.get("/home", authenticated, function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
+//Export router
 module.exports = router;
