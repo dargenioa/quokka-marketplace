79a870f2bfb5525254ede0085c4e0aec155115aa
Christopher-Lamb <chris.lamb13@hotmail.com>
2020-12-15 10:50:25 -0800

    File Uploades To DB

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-const express = require('express');
-const htmlRoutes = require('./routes/html-routes');
-const apiRoutes = require('./routes/api-routes');
-const db = require('./models');
-const passport = require('./config/passport');
+const express = require("express");
+const htmlRoutes = require("./routes/html-routes");
+const apiRoutes = require("./routes/api-routes");
+const db = require("./models");
+const passport = require("./config/passport");
 const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+//Server
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+//Passport
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Routes
 app.use(apiRoutes);
 app.use(htmlRoutes);
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function(){
-        console.log(`App listening on PORT ${PORT}`);
-    });
+//Multer
+global.__basedir = __dirname;
+
+//Sequelize
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log(`App listening on PORT ${PORT}`);
+  });
 });
