cb8d2b45e3d305c9e84abf6561158e50e66e195a
Gavin56 <gavinobrien56@gmail.com>
2020-12-19 13:30:01 -0500

    changes

--- public/js/home.js
+++ public/js/home.js
@@ -1,19 +1,42 @@
-$.ajax("/api/all-listings", {
+// $.ajax("/api/all-user", {
+//     type: "GET"
+// }).then(function (data) {
+//     //   res.json(data);
+//     for (i = 0; i < data.length; i++) {
+//         var rowIndex = i + 1;
+//         var userListing =
+
+//             `<tr>
+//                   <th scope="row">${rowIndex}</th>
+//                   <td>${data[i].name}</td>
+//                   <td>${data[i].price}</td>
+//                   <td>${data[i].quantity}</td>
+//                   <td><button class = "edit-listing">Edit</button></td>
+//               </tr>`
+
+//         $("#tableBody").append(userListing);
+//     };
+// });
+
+$.ajax("/api/all-users", {
     type: "GET"
 }).then(function (data) {
-    //   res.json(data);
+    console.log("all users", data);
     for (i = 0; i < data.length; i++) {
-        var rowIndex = i + 1;
-        var userListing =
+        for (i = 0; data.Listings.length; i++) {
+            var userListing =
 
             `<tr>
-                  <th scope="row">${rowIndex}</th>
-                  <td>${data[i].name}</td>
-                  <td>${data[i].price}</td>
-                  <td>${data[i].quantity}</td>
-                  <td><button class = "edit-listing">Edit</button></td>
-              </tr>`
+                <th scope="row">${rowIndex}</th>
+                <td>${data.Listings[i].name}</td>
+                <td><img class='listingThumbnail' src = '${data.Listings[i].url}'/></td>
+                <td>$${data.Listings[i].price}</td>
+                <td>${data.Listings[i].quantity}</td>
+                <td>${data.Listings[i].createdAt}
+                <td><button class = "edit-listing">Edit</button></td>
+            </tr>`
 
-        $("#tableBody").append(userListing);
+            $("#tableBody").append(userListing);
+        }
     };
 });
