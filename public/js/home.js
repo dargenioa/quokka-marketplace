7a238a0866a367fe381a34f9a5173c68ebc9797c
Amanda D'Argenio <dargenioa14@gmail.com>
2020-12-20 16:07:57 -0500

    added search by category and include all in search dropdown

--- public/js/home.js
+++ public/js/home.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     let index = 1;
 
     const generateListingTable = (data) => {
+        $("#tableBody").empty();
         for (let i = 0; i < data.length; i++) {
             let currentUser = data[i];
 
@@ -16,7 +17,7 @@ $(document).ready(function () {
                 <td><img class='listingThumbnail' src = '${currentUser.Listings[j].url}'/></td>
                 <td>$${currentUser.Listings[j].price}</td>
                 <td>${currentUser.Listings[j].quantity}</td>
-                <td id="category">${currentUser.Listings[j].category}</td>
+                <td>${currentUser.Listings[j].category}</td>
                 <td>${date}</td>
                 <td>${currentUser.username}</td>
                 <td><button type="button" class="btn btn-success">Purchase</button></td>
@@ -27,19 +28,47 @@ $(document).ready(function () {
         };
     };
 
+    const sortListings = (results) => {
+        $("#tableBody").empty();
+
+        for (let j = 0; j < results.length; j++) {
+            let rowIndex = index++;
+            let date = new Date(results[j].createdAt).toDateString();
+            let listing =
+
+                `<tr>
+        <th scope="row">${rowIndex}</th>
+        <td>${results[j].name}</td>
+        <td><img class='listingThumbnail' src = '${results[j].url}'/></td>
+        <td>$${results[j].price}</td>
+        <td>${results[j].quantity}</td>
+        <td>${results[j].category}</td>
+        <td>${date}</td>
+        <td>${results[j].username}</td>
+        <td><button type="button" class="btn btn-success">Purchase</button></td>
+    </tr>`
+
+            $("#tableBody").append(listing);
+        }
+    };
+
     $.ajax("/api/all-users", {
         type: "GET"
     }).then(generateListingTable);
 
-    $(".dropdown-item").on("click", function () {
-        let item = $(this).name("cat");
+    $(".cat").on("click", function () {
+        let item = $(this).data("cat");
         console.log(item);
 
-        // $.ajax("/api/category", {
-        //     type: "GET"
-        // }).then(console.log($(".dropdown-item")));
-    })
+        $.ajax("/api/category/" + item, {
+            type: "GET"
+        }).then(sortListings);
+    });
+
+    $("#includeAll").on("click", function () {    
+    $.ajax("/api/all-users", {
+        type: "GET"
+    }).then(generateListingTable);
 });
 
-
-
+});
