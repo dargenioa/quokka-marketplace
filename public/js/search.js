0a5ede56eba9c748c8165fdd451dbbb2ee91df7c
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-12-21 07:24:24 -0500

    functional searchbar. removed comments from failed attempts and kept relavent console logs. fixing front end format.

--- public/js/search.js
+++ public/js/search.js
@@ -1,62 +1,55 @@
 $(document).ready(function () {
     const searchBar = document.getElementById("search-bar");
-    let grabRes = [];
-    let grabImgUrl = [];
-    let grabPrice = [];
-    let grabQuantity = [];
-    let grabCategory = [];
-    let grabDate = [];
-    let grabUser = [];
-    console.log(searchBar);
+    let listingData = [];
+    //console.log(searchBar);
 
     $.ajax("/api/all-users", {
         type: "GET"
-    }).then(function(results){
-        console.log(results[1].Listings[0].url);
-        results.forEach(a => grabRes.push(a.Listings));
-        console.log
-        // results.forEach(a => a.Listings.forEach(b => grabPrice.push(b.price)));
-        // results.forEach(a => a.Listings.forEach(b => grabImgUrl.push(b.url)));
-        // results.forEach(a => a.Listings.forEach(b => grabQuantity.push(b.quantity)));
-        // results.forEach(a => a.Listings.forEach(b => grabCategory.push(b.category)));
-        // results.forEach(a => a.Listings.forEach(b => grabDate.push(b.date)));
-        // results.forEach(a => grabUser.push(a.username));
-        //console.log(grabRes); 
-        displaySearch()
+    }).then(function (results) {
+        //console.log(results[1].Listings[0].url);
+        // results.forEach(a => grabRes.push(a.Listings.name));
+        //console.log(grabRes);
+        listingData = [].concat(...[].concat(...results.map(result => result.Listings))); //use this to traverse the array of objects.
+
+        console.log(listingData); //returns results.Listings.name all of them found in the db!
     });
 
 
     searchBar.addEventListener("keyup", function (event) {
-        //console.log(event);
+        console.log(event);
         //console.log(event.target.value);
         //console.log(grabRes);
 
+        if( event.which == 8 && ( document.activeElement.id == 'search-bar') ){   
+            event.preventDefault();  
+            return false;   
+          } 
         const searchProduct = event.target.value.toLowerCase();
-        const filteredName = grabName.filter((item) => {
-            return item.toLowerCase().includes(searchProduct);
+        console.log(searchProduct);
+        const filteredName = listingData.filter((item) => {
+            return item.name.toLowerCase().includes(searchProduct);
         });
 
-        console.log(filteredName);
+        //console.log(filteredName);
 
-        //displayTable(filteredName,grabPrice, grabImgUrl, grabQuantity, grabCategory, grabDate, grabUser);
+        displayItems(filteredName);
     });
 
+    function displayItems(searchedItems){
+        $("#search-table").empty();
+        const htmlTable = searchedItems.map((item) => {
+            return `<tr>
+            //         <td>${item.name}</td>
+            //         <td><img class='listingThumbnail' src = '${item.url}'/></td>
+            //         <td>$${item.price}</td>
+            //         <td>${item.quantity}</td>
+            //         <td>${item.category}</td>
+            //         <td><button type="button" class="btn btn-success">Purchase</button></td>
+            //     </tr>`
+        });
 
-    // function displayTable(tableName, tableUrl, tablePrice, tableQuantity, tableCategory, tableDate, tableUser){
-    //     $("#search-table").empty();
-    //     let date = new Date(tableDate).toDateString();
-    //     const htmlTable = 
-    //          `<tr>
-    //         <td>${tableName}</td>
-    //         <td><img class='listingThumbnail' src = '${tableUrl}'/></td>
-    //         <td>$${tablePrice}</td>
-    //         <td>${tableQuantity}</td>
-    //         <td>${tableCategory}</td>
-    //         <td>${date}</td>
-    //         <td>${tableUser}</td>
-    //         <td><button type="button" class="btn btn-success">Purchase</button></td>
-    //     </tr>`
+        $("#search-table").append(htmlTable);
+    }
 
-    //     $("#search-table").append(htmlTable);
-    // }
+    displayItems(listingData)
 });
