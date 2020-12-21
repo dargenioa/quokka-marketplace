62e01e5dd14b1ca0c1686a2e88d62b03548f364e
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-12-21 06:08:41 -0500

    search implementation failed. new idea in the works.

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
     const searchBar = document.getElementById("search-bar");
     let grabRes = [];
+    let grabImgUrl = [];
+    let grabPrice = [];
+    let grabQuantity = [];
+    let grabCategory = [];
+    let grabDate = [];
+    let grabUser = [];
     console.log(searchBar);
 
     $.ajax("/api/all-users", {
         type: "GET"
     }).then(function(results){
-        //console.log(results);
-        results.forEach(a => a.Listings.forEach(b => grabRes.push(b.name)));
+        console.log(results[1].Listings[0].url);
+        results.forEach(a => grabRes.push(a.Listings));
+        console.log
+        // results.forEach(a => a.Listings.forEach(b => grabPrice.push(b.price)));
+        // results.forEach(a => a.Listings.forEach(b => grabImgUrl.push(b.url)));
+        // results.forEach(a => a.Listings.forEach(b => grabQuantity.push(b.quantity)));
+        // results.forEach(a => a.Listings.forEach(b => grabCategory.push(b.category)));
+        // results.forEach(a => a.Listings.forEach(b => grabDate.push(b.date)));
+        // results.forEach(a => grabUser.push(a.username));
         //console.log(grabRes); 
+        displaySearch()
     });
 
 
@@ -18,12 +32,31 @@ $(document).ready(function () {
         //console.log(grabRes);
 
         const searchProduct = event.target.value.toLowerCase();
-        const filteredName = grabRes.filter((item) => {
+        const filteredName = grabName.filter((item) => {
             return item.toLowerCase().includes(searchProduct);
         });
 
         console.log(filteredName);
+
+        //displayTable(filteredName,grabPrice, grabImgUrl, grabQuantity, grabCategory, grabDate, grabUser);
     });
 
 
+    // function displayTable(tableName, tableUrl, tablePrice, tableQuantity, tableCategory, tableDate, tableUser){
+    //     $("#search-table").empty();
+    //     let date = new Date(tableDate).toDateString();
+    //     const htmlTable = 
+    //          `<tr>
+    //         <td>${tableName}</td>
+    //         <td><img class='listingThumbnail' src = '${tableUrl}'/></td>
+    //         <td>$${tablePrice}</td>
+    //         <td>${tableQuantity}</td>
+    //         <td>${tableCategory}</td>
+    //         <td>${date}</td>
+    //         <td>${tableUser}</td>
+    //         <td><button type="button" class="btn btn-success">Purchase</button></td>
+    //     </tr>`
+
+    //     $("#search-table").append(htmlTable);
+    // }
 });
