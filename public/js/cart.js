8494d1776ac7bdd3ebce857cc3a90f61db647c9c
Christopher-Lamb <chris.lamb13@hotmail.com>
2020-12-21 14:41:19 -0800

    Cart Item Btns

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
-  $.get("/api/user").then(function (data) {
-    //Checks for cart Items
-    if (!data.cartItems) {
-      $("#tableDiv").text("You have no items in your cart.");
-    }
-    //Loop through Items
-    for (i = 0; i < data.cartItems.length; i++) {
-      let rowIndex = i + 1;
-      let date = new Date(data.cartItems[i].createdAt).toDateString();
-      let cartItem = `<tr>
+  let getCart = () => {
+    $.get("/api/user").then(function (data) {
+      //Checks for cart Items
+      if (!data.cartItems) {
+        $("#tableDiv").text("You have no items in your cart.");
+      }
+      //Loop through Items
+      for (i = 0; i < data.cartItems.length; i++) {
+        let rowIndex = i + 1;
+        let date = new Date(data.cartItems[i].createdAt).toDateString();
+        let cartItem = `<tr>
             <th scope="row">${rowIndex}</th>
             <td>${data.cartItems[i].name}</td>
             <td><img class='listingThumbnail' src = '${data.cartItems[i].url}'/></td>
@@ -16,12 +17,26 @@ $(document).ready(function () {
             <td>${data.cartItems[i].quantity}</td>
             <td>${data.cartItems[i].category}</td>
             <td>${date}</td>
-            <td><button type ="button" class = "edit-listing btn btn-warning" data-id="${data.cartItems[i].id}">Edit</button></td>
-            <td><button type="button" class="delete-listing btn btn-danger" data-id="${data.cartItems[i].id}">Delete</button></td>
+            <td><button type ="button" class = "edit-item btn btn-warning" data-id="${data.cartItems[i].id}">Edit</button></td>
+            <td><button type="button" class="delete-item btn btn-danger" data-id="${data.cartItems[i].id}">Delete</button></td>
         </tr>`;
 
-      $("#tableBody").append(cartItem);
-    }
-  });
-});
+        $("#tableBody").append(cartItem);
+      }
+    });
+  };
 
+  //Delete Btn
+  $(document).on("click", ".delete-item", function () {
+    let id = $(this).data("id");
+
+    $.ajax({
+      method: "DELETE",
+      url: "/api/cart-items/" + id,
+    }).then(function () {
+      // console.log("deleted");
+      getCart();
+    });
+  });
+  getCart();
+});
