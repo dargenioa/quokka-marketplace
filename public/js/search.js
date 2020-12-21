0af2cbf1ed00c95dbe4f0a78b304cc2ecec32bfa
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-12-21 05:00:30 -0500

    search bar now filters names and can bring them up when typing in the bar. going to take user on sell.html on submit.

--- public/js/search.js
+++ public/js/search.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
     const searchBar = document.getElementById("search-bar");
-    let nameFilter = [];
+    let grabRes = [];
     console.log(searchBar);
 
     $.ajax("/api/all-users", {
         type: "GET"
     }).then(function(results){
-        console.log(results);
+        //console.log(results);
+        results.forEach(a => a.Listings.forEach(b => grabRes.push(b.name)));
+        //console.log(grabRes); 
     });
 
 
     searchBar.addEventListener("keyup", function (event) {
         //console.log(event);
-        console.log(event.target.value);
-    })
+        //console.log(event.target.value);
+        //console.log(grabRes);
+
+        const searchProduct = event.target.value.toLowerCase();
+        const filteredName = grabRes.filter((item) => {
+            return item.toLowerCase().includes(searchProduct);
+        });
+
+        console.log(filteredName);
+    });
 
 
-})
+});
